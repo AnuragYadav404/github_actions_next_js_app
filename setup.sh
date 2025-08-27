c8366062b271b1a64007adbf5f0bf04ff7b04f62
AnuragYadav404 <124185505+AnuragYadav404@users.noreply.github.com>
2025-08-27 19:39:02 +0200

    Add logic to pull code from github if folder alreadt exists

--- setup.sh
+++ setup.sh
@@ -2,16 +2,25 @@
 set -e  # exit if any command fails
 
 APP_DIR="/var/www/myapp"
+REPO="https://github.com/AnuragYadav404/github_actions_next_js_app.git"
 
 echo "🚀 Starting deployment..."
 
-cd $APP_DIR
-
-
 export PATH=$HOME/.nvm/versions/node/v22.18.0/bin:$PATH
 
-# Pull latest code
-git clone "https://github.com/AnuragYadav404/github_actions_next_js_app.git"
+# Clone or pull repository
+if [ ! -d "$APP_DIR/.git" ]; then
+    echo "Repository not found. Cloning..."
+    git clone $REPO $APP_DIR
+else
+    echo "Repository exists. Pulling latest changes..."
+    cd $APP_DIR
+    git reset --hard          # Discard any local changes
+    git checkout main         # Ensure we're on main branch
+    git pull origin main
+fi
+
+cd $APP_DIR
 
 # Install only production dependencies
 npm install --production
